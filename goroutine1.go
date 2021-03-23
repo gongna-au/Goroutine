f1bad8bfa42863b2ba8009f9c2566db3d1aa893f
gongna-au <2036479155@qq.com>
2021-03-23 23:37:01 +0800

    build：some goroutine tests

---
+++ goroutine1.go
@@ -0,0 +1,22 @@
+package main
+import "fmt"
+func main()  {
+	done:= make(chan bool)
+	
+	
+	go func ()  {
+		fmt.Println("你好！并发")
+		//管道有数据时打印完成
+		done <- true
+
+	}()
+	
+        
+	<- done
+	//接收数据，完美退出
+}
+
+
+
+
+	
